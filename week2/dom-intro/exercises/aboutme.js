c8202ce62586a71e4872e2ca3609a6f2d5942ac1
SenorPepe92 <adenpetersen92@gmail.com>
2022-09-01 19:08:06 +0100

    DOM Exercises Booklist and aboutme

--- week2/dom-intro/exercises/aboutme.js
+++ week2/dom-intro/exercises/aboutme.js
@@ -1,5 +1,58 @@
 // Add a script tag to the bottom.
-// (In JS) Change the body tag's style so it has a font-family of "Arial, sans-serif".
-// (In JS) Replace each of the spans (nickname, favorites, hometown) with your own information.
-// Iterate through each li and change the class to "listitem". Add a style tag that sets a rule for "listitem" to make the color red.
-// Create a new img element and set its src attribute to a picture of you. Append that element to the page.
+
+// 1. (In JS) Change the body tag's style so it has a font-family of "Arial, sans-serif".
+
+// 2. (In JS) Replace each of the spans (nickname, favorites, hometown) with your own information.
+
+// 3. Iterate through each li and change the class to "listitem". Add a style tag that sets a rule for "listitem" to make the color red.
+
+// 4. Create a new img element and set its src attribute to a picture of you. Append that element to the page.
+
+// 1. 
+// const bodyText = document.getElementsByTagName('body')
+// console.log(bodyText);
+
+// bodyText.style.fontFamily = 'Arial'
+
+document.body.style.fontFamily = 'Arial, sans-serif'
+
+// 2. 
+// const liText = document.getElementsByTagName('li')
+// // console.log(liText);
+// liText[0].innerText += ' Pepe The Green Frog'
+// liText[1].innerText += ' Chilling in a pond'
+// liText[2].innerText += ' Sydney'
+
+const nicknameSpan = document.getElementById('nickname')
+// console.log(nicknameSpan);
+nicknameSpan.innerHTML += ' Pepe the frog'
+
+const favoritesSpan = document.getElementById('favorites')
+// console.log(nicknameSpan);
+favoritesSpan.innerHTML += ' Pizza'
+
+//The example below uses queryselector
+const hometownSpan = document.querySelector('#hometown')
+// console.log(nicknameSpan);
+hometownSpan.innerHTML += ' Tasmania'
+
+// 3. 
+const liItem = document.getElementsByTagName('li')
+// console.log(liItem.length - 1);
+for (i=0; i < liItem.length; i++) {
+    // See if the for loop works
+    const currentli = liItem[i];
+    console.log('current', currentli);
+    // add the class name using JS
+    liItem[i].className = 'listItem'
+}
+console.log(liItem); 
+// const styleliItems = document.querySelectorAll('.listItem')
+// styleliItems.style.c = 'red'
+// console.log(styleliItems);
+
+//4. Create a new img element and set its src attribute to a picture of you. Append that element to the page.
+
+const newImg = document.createElement('img'); 
+newImg.src = 'http://www.placekitten.com/100/100'; 
+document.body.appendChild(newImg)
---
+++ week2/dom-intro/exercises/booklist.js
@@ -0,0 +1,62 @@
+// Keep track of which books you read and which books you want to read!
+
+var books = [
+    {
+      title: 'The Design of EveryDay Things',
+      author: 'Don Norman',
+      alreadyRead: false
+    },
+    {
+      title: 'The Most Human Human',
+      author: 'Brian Christian',
+      alreadyRead: true
+    }
+  ];
+
+// Iterate through the array of books. For each book, create a p element with the book title and author and append it to the page.
+
+// Bonus: Use a ul and li to display the books.
+
+const ulContainer = document.createElement('ul'); 
+
+//   for (let i = 0; i < books.length; i++) {
+//     const bookTitle = books[i].title;
+//     console.log('Title:', bookTitle);
+//   }
+
+//   for (let i = 0; i < books.length; i++) {
+//     const bookTitle = books[i].title;
+//     // console.log('Title:', bookTitle);
+
+//     const newP = document.createElement('p');
+//     newP.innerHTML = `The title of the book is "${bookTitle}" and the author is ${books[i].author}`; 
+
+//     document.body.appendChild(newP) // added to the live DOM
+//   }
+
+  for (let i = 0; i < books.length; i++) {
+    const bookTitle = books[i].title;
+    // console.log('Title:', bookTitle);
+
+    const newli = document.createElement('li');
+    newli.innerHTML = `The title of the book is "${bookTitle}" and the author is ${books[i].author}`; 
+
+    if (books[i].alreadyRead) {
+        newli.className = "read"        
+    }
+    else { 
+        newli.className = "unread"
+    }
+
+    ulContainer.appendChild(newli)
+    // console.log(ulContainer);
+    // document.body.appendChild(newli) // added to the live DOM
+  };
+
+document.body.appendChild(ulContainer);
+
+
+
+
+// Bonus: add a property to each book with the URL of the book cover, and add an img element for each book on the page.
+// Bonus: Change the style of the book depending on whether you have read it or not.
